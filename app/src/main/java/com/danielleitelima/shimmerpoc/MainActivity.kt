56af2f435da3838877a41145c49b6fd28e597868
Daniel Leite Lima <limaleite.daniel@gmail.com>
2023-08-03 09:30:39 +0200

    Adds loader to the button

--- app/src/main/java/com/danielleitelima/shimmerpoc/MainActivity.kt
+++ app/src/main/java/com/danielleitelima/shimmerpoc/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.danielleitelima.shimmerpoc
 
+import android.content.res.ColorStateList
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
 import androidx.lifecycle.lifecycleScope
 import com.danielleitelima.shimmerpoc.databinding.ActivityMainBinding
 import com.faltenreich.skeletonlayout.createSkeleton
@@ -22,9 +25,19 @@ class MainActivity : AppCompatActivity() {
         binding.btnNext.setOnClickListener {
             lifecycleScope.launch {
                 // Simulate long running task
+                binding.btnText.isVisible = false
+                binding.progressCircular.isVisible = true
+                binding.btnNext.isEnabled = false
+                val lightGrayColor = ContextCompat.getColor(this@MainActivity, R.color.light_gray) // Replace 'your_color' with your color resource name
+                val blackColor = ContextCompat.getColor(this@MainActivity, R.color.black) // Replace 'your_color' with your color resource name
+                binding.btnNext.setCardBackgroundColor(ColorStateList.valueOf(lightGrayColor))
                 contentSkeleton.showSkeleton()
                 delay(3000)
+                binding.btnNext.setCardBackgroundColor(ColorStateList.valueOf(blackColor))
+                binding.btnNext.isEnabled = true
                 contentSkeleton.showOriginal()
+                binding.btnText.isVisible = true
+                binding.progressCircular.isVisible = false
             }
         }
     }
